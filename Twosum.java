4b2870601e9ea18639894df2a505b1a033cb23a7
MacBook Air <macbookair@MacBooks-MacBook-Air.local>
2026-05-04 23:48:08 +0530

    Day 2 - optimize code..

--- Twosum.java
+++ Twosum.java
@@ -10,26 +10,34 @@ Return the indices of the two numbers index1 and index2, each incremented by one
 The tests are generated such that there is exactly one solution. You may not use the same element twice.
 
 Your solution must use only constant extra space. */
-class Twosum {
 
+
+
+public class Twosum {
+
+    
     int[] twoSum(int[] numbers, int target) {
         
-        //int next=1;
+       int arr[] = new int[2];
+       int left = 0;
+       int right = numbers.length-1;
 
-        int arr[] = new int[2];
-      for(int i=0 ;i<numbers.length-1 ;i++){
+       for(int i=0 ;i<numbers.length ;i++){
 
-        for(int next = i+1; next < numbers.length ; next++){
+        if(numbers[left] + numbers[right] == target){
 
-            if (numbers[i] + numbers[next] == target) {
-                arr[0] = i+1;
-                arr[1] = next+1;
-            }
+            arr[0] = left+1;
+            arr[1] = right+1;
+
+            
+        }else if(numbers[left] + numbers[right] < target){
+            left++;
+        }else{
+            right --;
         }
 
-      }
-
-      return arr;  
+       }
+       return arr;
     }
 
     public static void main (String[] args){
@@ -42,6 +50,4 @@ class Twosum {
             System.out.print(resultarr[i] + "  ");
         }
     }
-    
-    
 }
